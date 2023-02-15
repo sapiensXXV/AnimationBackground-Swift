02268b812c90f4a13a78902cabfa11a52f2abe0e
Jaehoon-So <jhspacelover@naver.com>
2023-02-15 17:18:42 +0900

    add: add package files

---
+++ AnimationBackground/Sources/AnimationBackground/ABRainingView.swift
@@ -0,0 +1,20 @@
+//
+//  ABRainingView.swift
+//  
+//
+//  Created by Jaehoon So on 2023/02/15.
+//
+
+import UIKit
+
+public final class ABRainingView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
